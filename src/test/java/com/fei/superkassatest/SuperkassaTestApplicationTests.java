f779989f1e67ddf14a0d8c5d901621c4f11d0f30
Olga Fedorova <murai.kai@gmail.com>
2023-02-05 11:47:55 +0300

    Initial commit

---
+++ src/test/java/com/fei/superkassatest/SuperkassaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fei.superkassatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperkassaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
